8e4b5c7d96f1cbb239ed7c43c06187ce8188e039
Mr-Krab <StalkerIv@yandex.ru>
2017-08-28 16:48:02 +0300

    Предрелизное обновление, не для использования

--- src/mr_krab/customjoinstream/Plugin.java
+++ src/mr_krab/customjoinstream/Plugin.java
@@ -1,8 +1,10 @@
 package mr_krab.customjoinstream;
 
+import java.lang.reflect.Field;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -35,17 +37,25 @@ public class Plugin extends JavaPlugin implements Listener {
 	public void onEnable() {
 		saveDefaultConfig();
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		/* Активируем рефлексию Java для регистрации команд
+		 * Регистрация производится через класс CommandRegister(не изменять его)
+		 * Сами команды прописывать как обычно в CmdExecutor
+		 */
+		try {
+            CmdExecutor cmd = new CmdExecutor(this);
+            CommandRegister reg = new CommandRegister(new String[]{"customjoinstream"}, "Используйте", "customjoinstream или cjs", cmd, new Object(), this);
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            CommandMap map = (CommandMap)field.get(Bukkit.getServer());
+            map.register(this.getDescription().getName(), reg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	    setupChat();
-		CmdExecutor cmde = new CmdExecutor(this);
-		String[] comands = { "cjs" };
-		for (String cmd : comands) {
-			getCommand(cmd).setExecutor(cmde);
-}
 		mclog.info("Плагин [CustomJoinStream] активирован");
 		mclog.info("Автор Mr_Krab");
 		mclog.info("Спасибо за использование и тестирование моих плагинов");
 	}
-	
 	// Выключение плагина
 	public void onDisable() {
 		mclog.info("Плагин [CustomJoinStream] отключается");
