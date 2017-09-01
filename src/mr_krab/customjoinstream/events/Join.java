1b9ec7ede317a145d1e974b6207cd94f23396422
Mr-Krab <StalkerIv@yandex.ru>
2017-09-01 16:47:32 +0300

    Добавлено воспроизведение звука. Нужно доработать.

--- src/mr_krab/customjoinstream/events/Join.java
+++ src/mr_krab/customjoinstream/events/Join.java
@@ -1,5 +1,6 @@
 package mr_krab.customjoinstream.events;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,21 +15,32 @@ public class Join implements Listener {
 	      instance = plugin;
 	}
 	
+
 	// Скрытие стандартного сообщения о входе игрока
+		@EventHandler(priority = EventPriority.HIGHEST)
+	    public boolean broadcastSound(PlayerJoinEvent je) {
+	        je.setJoinMessage(null); {
+	        // Проверка на наличие пермишена и вывод своего сообщения
+	        Player p = je.getPlayer();
+	        String group = Plugin.c.getPrimaryGroup(p);
+	        String prefix = Plugin.c.getGroupPrefix(p.getWorld(), group); 
+	        String suffix = Plugin.c.getGroupSuffix(p.getWorld(), group);
+			if(!p.hasPermission("customjoinstream.stream")) {
+				} else {
+						je.setJoinMessage(instance.getConfig().getString("Messages.JoinMessage").replace("%prefix", prefix).replace("%nickname", je.getPlayer().getName()).replace("%suffix", suffix).replace("&", "§"));
+					return true;
+				}
+				return false;
+	        }	
+		}
+	
+	// Воспроизведение звука при входе
 	@EventHandler(priority = EventPriority.HIGHEST)
-    public boolean JoinUpdate(PlayerJoinEvent je) {
-        je.setJoinMessage(null); {
-        // Проверка на наличие пермишена и вывод своего сообщения
-        Player p = je.getPlayer();
+    public boolean JoinSound(PlayerJoinEvent jes) {
+        Player p = jes.getPlayer();
         String group = Plugin.c.getPrimaryGroup(p);
-        String prefix = Plugin.c.getGroupPrefix(p.getWorld(), group); 
-        String suffix = Plugin.c.getGroupSuffix(p.getWorld(), group); 
-		if(!p.hasPermission("customjoinstream.stream")) {
-			} else {
-					je.setJoinMessage(instance.getConfig().getString("Messages.JoinMessage").replace("%prefix", prefix).replace("%nickname", je.getPlayer().getName()).replace("%suffix", suffix).replace("&", "§"));
-				return true;
-			}
-		return false;
-        }
-	}	
+		if(p.hasPermission("customjoinstream.sound"))
+			p.playSound(p.getLocation(), Sound.valueOf(instance.config.getString("Groups." + group + ".sound").toUpperCase()), 10, 100);
+		return true;
+	}
 }
